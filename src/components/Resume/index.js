f0cdc45f6710d7aa14e42f1639fbdbd0cbf37e19
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2024-01-27 11:48:06 -0300

    corrigindo

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -86,7 +86,7 @@ export default function Resume() {
               mini-projects
             </p>
             <a
-              href="#"
+              href="https://www.google.com.br/"
               target="_blank"
               rel="noreferrer"
               class="inline-flex items-center px-4 py-2 mt-5 text-sm font-medium border rounded-lg  focus:z-10 focus:ring-4 focus:outline-none focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700"
